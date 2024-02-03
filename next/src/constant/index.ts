f8d68537ab6608a7b080cbecf85b14cda5ffca94
shuhrat567 <isoilovshuhrat567@gmail.com>
2024-02-03 13:07:45 +0300

    first commit next navbar

---
+++ next/src/constant/index.ts
@@ -0,0 +1,8 @@
+export const NavLinks = [
+    { href: '/', key: 'Home', text: 'Home' },
+    { href: '/', key: 'Our Services', text: 'Our Services' },
+    { href: '/', key: 'Projects', text: 'Projects' },
+    { href: '/', key: 'Blog', text: 'Blog' },
+    { href: '/', key: 'About', text: 'About' }
+  ];
+  
